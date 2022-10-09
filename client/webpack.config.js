490d6e1d2e3bde8327d3decf9c234fd301822bc4
kea6t <keasay26@gmail.com>
2022-10-09 15:58:19 -0400

    got the app working in localhost, trying to deploy to heroku for production

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -43,7 +43,7 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
+        swDest: 'src-sw.js',
       }),
     ],
     // TODO: Add CSS loaders and babel to webpack.
