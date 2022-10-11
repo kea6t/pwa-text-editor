a9852d477ec0799104bfc5d056a505aacc8fed90
kea6t <keasay26@gmail.com>
2022-10-10 23:28:08 -0400

    adjusted the database.js to show the header for jate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -57,7 +57,8 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
+
